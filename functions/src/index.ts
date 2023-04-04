c142f616cc538442979dc392530be5d0f2b0e835
Adam Kubina <adam.kubinaa@gmail.com>
2023-04-04 21:31:30 +0200

    Created delete book offer

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -41,13 +41,59 @@ functions.runWith({enforceAppCheck: true})
     const pathUser = admin.firestore().collection("/users/" +
     context.auth.uid + "/bookOffers").doc();
     const pathBook = admin.firestore().collection("/books/" +
-    bookId + "/bookOffers").doc();
+    bookId + "/bookOffers").doc(pathUser.id);
 
 
     batch.set(pathUser, {bookId: bookId,
       bookState: bookState, notes: offerNotes});
     batch.set(pathBook, {userId: context.auth.uid,
       bookState: bookState, notes: offerNotes});
+
+
+    batch.commit();
+  });
+
+export const deleteBookOffer =
+functions.runWith({enforceAppCheck: true})
+  .https.onCall((data, context) => {
+    if (context.app == undefined) {
+      throw new functions.https.HttpsError(
+        "failed-precondition",
+        "The function must be called from App check verified app."
+      );
+    }
+    if (!context.auth) {
+      throw new functions.https.HttpsError("failed-precondition",
+        "The function must be called by authenticated user.");
+    }
+
+    const bookOfferId = data?.bookOfferId;
+
+    if (!(typeof bookOfferId === "string") || bookOfferId.length < 3 ||
+    bookOfferId.length > 24) {
+      throw new functions.https.HttpsError("invalid-argument",
+        "The function must be called must be called with argument" +
+        "bookOfferId as a valid string id.");
+    }
+
+    const bookId = data?.bookId;
+
+    if (!(typeof bookId === "string") || bookId.length < 3 ||
+    bookId.length > 24) {
+      throw new functions.https.HttpsError("invalid-argument",
+        "The function must be called must be called with argument" +
+        "bookId as a valid string id.");
+    }
+
+    const batch = admin.firestore().batch();
+    const pathUser = admin.firestore().doc("/users/" +
+    context.auth.uid + "/bookOffers/" + bookOfferId);
+    const pathBook = admin.firestore().doc("/books/" +
+    bookId + "/bookOffers/" + bookOfferId);
+
+
+    batch.delete(pathUser);
+    batch.delete(pathBook);
 
 
     batch.commit();
