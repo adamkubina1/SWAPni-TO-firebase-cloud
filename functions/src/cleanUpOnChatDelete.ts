41561db71ffcc87a990b3de48027c0985057ad13
Adam Kubina <adam.kubinaa@gmail.com>
2023-04-21 11:07:55 +0200

    Added chunks

--- functions/src/cleanUpOnChatDelete.ts
+++ functions/src/cleanUpOnChatDelete.ts
@@ -1,19 +1,37 @@
 import * as admin from "firebase-admin";
+import {WriteResult} from "firebase-admin/firestore";
 import * as functions from "firebase-functions";
+import * as _ from "lodash";
+
+const MAX_BATCH_SIZE = 500;
 
 export const cleanUpOnChatDelete =
 functions.region("europe-west3").firestore
   .document("chats/{docId}").onDelete(async (snap) => {
     const deletedDocId = snap.id;
-    const batch = admin.firestore().batch();
 
     const messages = await admin.firestore()
       .collection("chats/" + deletedDocId + "/messages")
       .get();
 
-    messages.forEach((bookOffer) => {
-      batch.delete(bookOffer.ref);
+    if (messages.empty) return;
+
+    const chunkedMessages = _.chunk(messages.docs, MAX_BATCH_SIZE);
+    const batchPromises:Array<Promise<WriteResult[]>> = [];
+
+    chunkedMessages.forEach((chunk) => {
+      const batch = admin.firestore().batch();
+
+      chunk.forEach((message) => batch.delete(message.ref));
+      batchPromises.push(new Promise(() => batch.commit()));
     });
 
-    batch.commit();
+    if (batchPromises.length < 1) return;
+
+    Promise.all(
+      batchPromises
+    ).catch(() => {
+      throw new
+      functions.https.HttpsError("internal", "Batch delete operation failed.");
+    });
   });
